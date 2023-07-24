ad6f71bca031dafc6d979e03f77226d391eef61d
HowToBasicDD <benefit5758@naver.com>
2023-07-24 14:02:29 +0900

    [Refactor] nest g co를 통한 컨트롤러 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MoviesController } from './movies/movies.controller';
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController],
+  // Express의 Router같은 존재
+  providers: [],
 })
 export class AppModule {}
